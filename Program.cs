7fbf03ad1220ab8eaa47ab2765945d7e812f2adc
fatih-git <72505641+fatih-git@users.noreply.github.com>
2022-10-28 02:06:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ForLoop
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            for(int i=0; i<10; i++)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+
+            //Sonsuz döngü
+            /*for(; ; )
+            {
+                Console.WriteLine("adsmfd");
+            }*/
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i <= 10; i++)
+            {
+
+                if (i == 10)
+                {
+                    Console.Write("Breaking!");
+                    break;
+                }
+
+                Console.Write(i + " ");
+            }
+
+        }
+
+    }
+
+}
